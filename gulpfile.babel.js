b70adc5eb61491b98246eddeb18d0d5408e01787
Pablo Peña <pablopenia.dev@gmail.com>
2022-09-16 15:39:22 -0300

    added browsersync for development

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import { src, dest, watch, series, parallel } from 'gulp'
 import autoprefixer from 'autoprefixer'
+import browserSync from 'browser-sync'
 import concat from 'gulp-concat'
 import dartSass from 'sass'
 import gulpSass from 'gulp-sass'
@@ -14,66 +15,96 @@ import terser from 'gulp-terser'
 const sass = gulpSass(dartSass)
 
 const paths = {
-  assets: {
-    src: './src/images/**/*',
-    dest: './dist/assets/images/',
-  },
-  scripts: {
-    src: './src/scripts/**/_*.js',
-    dest: './dist/assets/',
-    srcVendor: './src/scripts/**/!(_)*.js',
-    destVendor: './dist/assets/',
-  },
-  svg: './src/svg/**/*.svg',
-  theme: {
-    src: './src/theme/**/*.{scss,sass}',
-    dest: './dist/assets/',
-  },
-  template: {
-    src: './src/template/!(_)*.pug',
-    dest: './dist/',
-    watch: './src/template/**/*.pug',
-  },
+	assets: {
+		src: './src/images/**/*',
+		dest: './dist/assets/images/',
+	},
+	scripts: {
+		src: './src/scripts/**/_*.js',
+		dest: './dist/assets/',
+		srcVendor: './src/scripts/**/!(_)*.js',
+		destVendor: './dist/assets/',
+	},
+	svg: './src/svg/**/*.svg',
+	theme: {
+		src: './src/theme/**/*.{scss,sass}',
+		dest: './dist/assets/',
+	},
+	template: {
+		src: './src/template/!(_)*.pug',
+		dest: './dist/',
+		watch: './src/template/**/*.pug',
+	},
+	dist: './dist/',
 }
 
+const startServer = () =>
+	browserSync.init({
+		server: {
+			baseDir: `${paths.dist}`,
+		},
+	})
+
 function pageBuilder() {
-  return src(paths.template.src)
-    .pipe(plumber())
-    .pipe(pug({ pretty: true }))
-    .pipe(dest(paths.template.dest))
+	return src(paths.template.src)
+		.pipe(plumber())
+		.pipe(pug({ pretty: true }))
+		.pipe(dest(paths.template.dest))
+		.pipe(browserSync.reload({ stream: true }))
 }
 
 const copyAssets = () => src(paths.assets.src).pipe(dest(paths.assets.dest))
-const copyLib = () => src(paths.scripts.srcVendor).pipe(dest(paths.scripts.destVendor))
+const copyLib = () =>
+	src(paths.scripts.srcVendor).pipe(dest(paths.scripts.destVendor))
 
 function scriptsBuilder() {
-  return src(paths.scripts.src)
-    .pipe(concat('app.js'))
-    .pipe(init())
-    .pipe(terser().on('error', error => console.log(error)))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(write('.'))
-    .pipe(dest(paths.scripts.dest))
+	return src(paths.scripts.src)
+		.pipe(concat('app.js'))
+		.pipe(init())
+		.pipe(terser().on('error', (error) => console.log(error)))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(write('.'))
+		.pipe(dest(paths.scripts.dest))
 }
 
 function themeBuilder() {
-  const plugins = [postcssNormalize(), autoprefixer(), cssnano()]
-  return src(paths.theme.src)
-    .pipe(init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(plugins))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(write('.'))
-    .pipe(dest(paths.theme.dest))
+	const plugins = [postcssNormalize(), autoprefixer(), cssnano()]
+	return src(paths.theme.src)
+		.pipe(init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(plugins))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(write('.'))
+		.pipe(dest(paths.theme.dest))
+		.pipe(browserSync.reload({ stream: true }))
 }
 
+const watchJs = () => watch(paths.scripts.src, scriptsBuilder)
+const watchJsVendor = () => watch(paths.scripts.srcVendor, copyLib)
+const watchSvg = () => watch(paths.svg, pageBuilder)
+const watchAssets = () =>
+	watch(paths.assets.src, series(copyAssets, pageBuilder))
+const watchStyles = () => watch(paths.theme.src, themeBuilder)
+const watchViews = () => watch(paths.template.watch, pageBuilder)
 function watcher() {
-  watch(paths.scripts.src, scriptsBuilder)
-  watch(paths.scripts.srcVendor, copyLib)
-  watch(paths.svg, pageBuilder)
-  watch(paths.assets.src, series(copyAssets, pageBuilder))
-  watch(paths.theme.src, themeBuilder)
-  watch(paths.template.watch, pageBuilder)
+	startServer()
 }
-exports.build = parallel(copyAssets, copyLib, scriptsBuilder, pageBuilder, themeBuilder)
-exports.default = series(parallel(copyAssets, copyLib, scriptsBuilder, pageBuilder, themeBuilder), watcher)
+
+const watchTask = parallel(
+	startServer,
+	watchJs,
+	watchJsVendor,
+	watchSvg,
+	watchAssets,
+	watchStyles,
+	watchViews
+)
+const buildTask = parallel(
+	copyAssets,
+	copyLib,
+	scriptsBuilder,
+	pageBuilder,
+	themeBuilder
+)
+exports.build = buildTask
+exports.default = series(buildTask, watchTask)
